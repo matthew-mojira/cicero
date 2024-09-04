369a788a91abb9eaaa1915d1556abe87f7d67a46
Matthew Schneider <mschnei2@andrew.cmu.edu>
2024-09-03 22:30:57 -0400

    Reorganized main

--- app/AST.hs
+++ app/AST.hs
@@ -4,27 +4,54 @@ import Value
 
 type Prog = Expr
 
-data Expr = ExprLit Value
-          | ExprUnOp UnOp Expr
-          | ExprBinOp BinOp Expr Expr
+data Expr
+  = ExprLit Value
+  | ExprUnOp UnOp Expr
+  | ExprBinOp BinOp Expr Expr
 
-data UnOp = LNot
+data UnOp =
+  LNot
 
-data BinOp = Add | Sub | Mult | Div | LAnd | LOr
+data BinOp
+  = Add
+  | Sub
+  | Mult
+  | Div
+  | LAnd
+  | LOr
+  | Eq
+  | Neq
+  | Le
+  | Leq
+  | Ge
+  | Geq
 
 binOpInt :: BinOp -> Bool
-binOpInt Add = True
-binOpInt Sub = True
+binOpInt Add  = True
+binOpInt Sub  = True
 binOpInt Mult = True
-binOpInt Div = True
-binOpInt _ = False
+binOpInt Div  = True
+binOpInt _    = False
+
+binOpEq :: BinOp -> Bool
+binOpEq Eq  = True
+binOpEq Neq = True
+binOpEq _   = False
+
+binOpComp :: BinOp -> Bool
+binOpComp Le  = True
+binOpComp Leq = True
+binOpComp Ge  = True
+binOpComp Geq = True
+binOpComp _   = False
 
 instance Show Expr where
   show (ExprLit val) = show val
-  show (ExprBinOp op expr1 expr2) = "(" ++ show expr1 ++ " " ++ show op ++ " " ++ show expr2 ++ ")"
+  show (ExprBinOp op expr1 expr2) =
+    "(" ++ show expr1 ++ " " ++ show op ++ " " ++ show expr2 ++ ")"
 
 instance Show BinOp where
-  show Add  = "+"
-  show Sub  = "-"
+  show Add = "+"
+  show Sub = "-"
   show Mult = "*"
-  show Div  = "/"
+  show Div = "/"
--- app/Main.hs
+++ app/Main.hs
@@ -5,17 +5,30 @@ import Typechecker
 
 import Eval
 
-import System.IO
 import Control.Monad
+import System.Console.Haskeline
+import System.Environment
 
 main :: IO ()
-main = forever $ do
-  putStr "mpl> "
-  hFlush stdout
-  prog <- getLine
+main = do
+  args <- getArgs
+  runInputT defaultSettings loop
 
+loop :: InputT IO ()
+loop = do
+  minput <- getInputLine "mpl> "
+  case minput of
+    Nothing -> return ()
+    Just ":quit" -> return ()
+    Just prog -> do
+      execProg prog
+      loop
+
+execProg :: String -> InputT IO ()
+execProg prog =
   case parse prog of
-    Left msg  -> putStrLn msg
-    Right ast -> case typeof ast of
-                   Left msg -> putStrLn msg
-                   Right _  -> print $ eval ast
+    Left msg -> outputStrLn msg
+    Right ast ->
+      case typeof ast of
+        Left msg -> outputStrLn msg
+        Right _ -> outputStrLn $ show $ eval ast
