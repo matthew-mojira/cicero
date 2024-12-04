9a54f5bee8708e2e514de0c8390f9dbf805a4523
Matthew Schneider <panat.matthew@gmail.com>
2024-12-04 16:43:12 -0500

    haskell testing

---
+++ app/Test.hs
@@ -0,0 +1,8 @@
+-- assert
+--	String : input program
+--	Value  : expected value
+-- result
+--  Value  : failed, actual output
+--  ()     : passed
+assert :: String -> Value -> IO (Either Value ())
+assert = undefined
