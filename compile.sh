72722badceabd13af88b39a90b2743f3489d5613
Matthew Schneider <panat.matthew@gmail.com>
2025-04-15 15:49:22 -0400

    (partial) tier1 bytecode, compiler, and evaluator

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-v3c-x86-64-linux -O3 ~/virgil/lib/util/*.v3 src/*.v3 src/util/*.v3 $1
+v3c-wasm-wave -heap-size=1900M -O3 -symbols ~/virgil/lib/util/*.v3 src/*.v3 src/util/*.v3 $1
+v3c-x86-64-linux -O3 -symbols ~/virgil/lib/util/*.v3 src/*.v3 src/util/*.v3 $1
