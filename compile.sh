471253136a4be9dfdebc5214f9ee71507e7491ba
Matthew Schneider <panat.matthew@gmail.com>
2025-04-16 11:02:13 -0400

    Optimizations

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
-v3c-wasm-wave -heap-size=1900M -O3 -symbols ~/virgil/lib/util/*.v3 src/*.v3 src/util/*.v3 $1
-v3c-x86-64-linux -O3 -symbols ~/virgil/lib/util/*.v3 src/*.v3 src/util/*.v3 $1
+v3c-wasm-wave -heap-size=1900M -O3 -symbols ~/virgil/lib/util/*.v3 src/*.v3 src/util/*.v3
+v3c-x86-64-linux -O3 -symbols ~/virgil/lib/util/*.v3 src/*.v3 src/util/*.v3
+# for very fast compilation:  -opt=+DisableBoundsChecks,+DisableNullChecks
