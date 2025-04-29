ca7e981409503e2d104448e71391e15dfa90106b
Matthew Schneider <panat.matthew@gmail.com>
2025-04-29 16:01:38 -0400

    Profile bytecodes in Cicero, to compare with GRP

--- wasm/grp.sh
+++ wasm/grp.sh
@@ -1,1 +1,1 @@
-(cd .. && wizeng --monitors=profile_bytecode{switch_size=10} bin/cicero.wasm -tier=1 -suppress-output examples/$1)
+(cd .. && wizeng --monitors=profile_bytecode{switch_size=20} bin/cicero.wasm -tier=1 -suppress-output examples/$1)
