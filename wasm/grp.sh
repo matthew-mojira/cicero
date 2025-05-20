38aff9f64bd58f20a78728787ab7880f97c3e8a5
Matthew Schneider <panat.matthew@gmail.com>
2025-05-20 17:11:39 -0400

    tier2 bytecode: the same except it uses LEBs for operands

--- wasm/grp.sh
+++ wasm/grp.sh
@@ -1,1 +1,1 @@
-(cd .. && wizeng --monitors=profile_bytecode{switch_size=20} bin/cicero.wasm -tier=1 -suppress-output examples/$1)
+(cd .. && wizeng --monitors=profile_bytecode{switch_size=20} bin/cicero.wasm -tier=$1 -suppress-output examples/$2)
