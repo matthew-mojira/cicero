f9d7cc55914a99bfc2f816e8410eb2437fbc343f
Matthew Schneider <panat.matthew@gmail.com>
2026-07-15 18:41:01 -0400

    Do not use `-symbols` by default.

--- build.sh
+++ build.sh
@@ -98,7 +98,7 @@ echo "}" >> "$CICERO_TEXT"
 PREGEN=${PREGEN:=1}
 
 LANG_OPTS="-simple-bodies -fun-exprs"
-V3C_OPTS="$V3C_OPTS -symbols -shadow-stack-size=10M -heap-size=1500M -stack-size=16M"
+V3C_OPTS="$V3C_OPTS -shadow-stack-size=10M -heap-size=1500M -stack-size=16M"
 
 # build
 exe=${PROGRAM}.${TARGET}
